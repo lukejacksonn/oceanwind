a02048b39e8cd29220f9bd51a736228f70370212
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-10 10:07:24 +0200

    chore: Set up initial SSR test
    Looks like this one fails at otion.

--- test/ssr.test.js
+++ test/ssr.test.js
@@ -1,5 +1,15 @@
+import { themed } from '../index.js';
+
 describe('ssr', () => {
   test('renders server side', () => {
-    console.log('renders server side')
+    const html = htm.bind(h);
+
+    const ow = themed({});
+
+    const style = {
+      main: ow`clearfix`,
+    }
+    
+    console.log('renders server side', style)
   })
 })
