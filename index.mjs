0b56d1ee9938e33f565ea390c566da337a18e55a
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-10 14:50:13 +0200

    refactor: Simplify tests to a simple assert
    Given we're using experimental module support of Node, I had to rename
    the files to use the mjs extension.

---
+++ index.mjs
@@ -0,0 +1,55 @@
+import { css } from "otion";
+import translate from "./core/translate.mjs";
+import defaultTheme from "./core/theme.mjs";
+import merge from "./util/merge.mjs";
+
+export { setup, hydrate } from "otion";
+
+export const process = (theme) => ([rules]) => {
+  // Keep track of processed rules
+  const seen = {};
+  // Go through each rule in the array and translate to css
+  const styles = rules
+    .replace(/\s\s+/g, " ")
+    .trim()
+    .split(" ")
+    .map((rule) => {
+      // Warn about any duplicate rule declarations
+      if (seen[rule]) console.warn(`Duplicate delclaration of ${rule}`);
+      // Mark rule as seen
+      seen[rule] = true;
+      // Split the rule into parts
+      rule = rule.split(":");
+      // Seperate out directive from variants
+      let directive = rule.pop();
+      let variants = rule;
+      // Lookup translation for directive
+      let translation = translate(theme)(directive);
+      // Warn if there was no translation for the given directive
+      if (!Object.keys(translation)[0]) {
+        console.warn(`No translation for ${directive}`);
+      }
+      // Apply variants to the translation
+      variants.reverse().forEach((variant) => {
+        let size = theme.screen[variant];
+        if (size) {
+          translation = {
+            "@media": { [`(min-width: ${size})`]: translation },
+          };
+        } else translation = { [`:${variant}`]: translation };
+      });
+      // Return translation with variants applied
+      return translation;
+    });
+  // Return class names for styles
+  return merge(...styles);
+};
+
+// If passed a theme then return process primed with merged themes
+export const themed = (input) => {
+  const theme = merge(defaultTheme, input);
+  return (input) => css(process(theme)(input));
+};
+
+// If passed a tagged template then process with the default theme
+export default (input) => css(process(defaultTheme)(input));
---
+++ test/ssr.test.mjs
@@ -0,0 +1,32 @@
+import assert from "assert";
+import { h } from "preact";
+import render from "preact-render-to-string";
+import htm from "htm";
+import {
+  filterOutUnusedRules,
+  getStyleTag,
+  VirtualInjector,
+} from "otion/server";
+import { setup, themed } from "../index.mjs";
+
+function testSsr() {
+  // https://www.npmjs.com/package/otion#server-side-rendering
+  const injector = VirtualInjector();
+  setup({ injector });
+
+  const html = htm.bind(h);
+  const ow = themed({});
+  const style = {
+    main: ow`clearfix`,
+  };
+
+  const app = html`<main className=${style.main}>hello oceanwind</main>`;
+  const appHtml = render(app);
+  const styleTag = getStyleTag(filterOutUnusedRules(injector, appHtml));
+
+  console.log("Generated a style tag", styleTag);
+
+  assert(styleTag);
+}
+
+testSsr();
