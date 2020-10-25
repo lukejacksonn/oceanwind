4cc0a43eba8071cab3e7d8a1e70a197bb3600e3b
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2020-10-25 05:54:46 -0500

    Adding TS types for pkg exports (#25)
    * feat: Adding TS types for pkg exports
    
    * fix: Correcting Styles type with TemplateStringsArray

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+type Styles = string | TemplateStringsArray | string[] | Record<string, boolean>;
+
+declare function process(theme: Record<string, unknown>): (strings: Styles) => Record<string, unknown>;
+declare function configure(theme: Record<string, unknown>): Record<string, unknown>;
+declare function themed(theme?: Record<string, unknown>): (strings: Styles) => string;
+declare function _default(strings: Styles): string;
+
+declare function hydrate(): void;
+declare function setup(options: {
+    injector?: {
+        sheet?: CSSStyleSheet;
+        insert(rule: string, index: number): number;
+        nonce: string | undefined;
+        ruleTexts: string[];
+    };
+    prefix?: string;
+}): void;
+
+export { process, configure, themed, hydrate, setup };
+export default _default;
