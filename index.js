cef4135575d5f88bf9ce9646df1933c1fb5eada5
lukejacksonn <lukejacksonn@gmail.com>
2020-11-26 21:43:29 +0000

    Adds caching for rule sets and rule translations

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import translate from './translate.js';
+import dictionary from './translate.js';
 import defaultTheme, { globals } from './theme.js';
 
 import merge from './util/merge.js';
@@ -16,42 +16,50 @@ const warn = (theme) => (message) => {
   console.warn(msg);
 };
 
-export const process = (theme) => (strings, values) => {
+const translate = (theme, rule) => {
+  // Split the rule into parts
+  rule = rule.split(':');
+  // Seperate out directive from variants
+  let directive = rule.pop();
+  let variants = rule;
+  // Lookup translation in dictionary
+  let translation = dictionary(theme)(directive);
+  // Warn if there was no translation for the given directive
+  if (!Object.keys(translation)[0] || !Object.values(translation)[0]) {
+    warn(theme)(`No translation for "${directive}"`);
+  }
+  // Apply variants to the translation
+  variants.reverse().forEach((variant) => {
+    let size = theme.screens[variant];
+    if (size) {
+      translation = {
+        '@media': { [`(min-width: ${size})`]: translation },
+      };
+    } else translation = { [`:${variant}`]: translation };
+  });
+  return translation;
+};
+
+export const process = (theme, cache = {}) => (strings, values) => {
   // Normalize rules into an array
   const rules = normalize(strings, values);
   // Keep track of processed rules this rule set
   const seen = {};
   // Go through each rule in the array and translate to css
-  const styles = rules.map((rule) => {
-    // Warn about any duplicate rule declarations
-    if (seen[rule]) warn(theme)(`Duplicate declaration of "${rule}"`);
-    // Mark rule as seen
-    seen[rule] = true;
-    // Split the rule into parts
-    rule = rule.split(':');
-    // Seperate out directive from variants
-    let directive = rule.pop();
-    let variants = rule;
-    // Lookup translation for directive
-    let translation = translate(theme)(directive);
-    // Warn if there was no translation for the given directive
-    if (!Object.keys(translation)[0] || !Object.values(translation)[0]) {
-      warn(theme)(`No translation for "${directive}"`);
-    }
-    // Apply variants to the translation
-    variants.reverse().forEach((variant) => {
-      let size = theme.screens[variant];
-      if (size) {
-        translation = {
-          '@media': { [`(min-width: ${size})`]: translation },
-        };
-      } else translation = { [`:${variant}`]: translation };
-    });
-    // Return translation with variants applied
-    return translation;
-  });
-  // Deep merge all translations
-  return merge(...styles);
+  return (cache[rules] =
+    cache[rules] ||
+    merge(
+      ...rules.map((rule) => {
+        // Warn about any duplicate rule declarations
+        if (seen[rule]) warn(theme)(`Duplicate declaration of "${rule}"`);
+        // Mark rule as seen
+        seen[rule] = true;
+        // Lookup translation for directive
+        let translation = (cache[rule] = cache[rule] || translate(theme, rule));
+        // Return translation with variants applied
+        return translation;
+      })
+    ));
 };
 
 // Utility for merging a provided theme with the default theme
